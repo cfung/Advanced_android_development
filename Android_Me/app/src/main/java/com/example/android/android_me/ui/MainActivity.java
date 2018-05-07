34bdb623b40518c22b766b7b63be452f212c1c6c
Chester Fung <cfung@netflix.com>
2018-05-06 23:22:17 -0700

    add main activity

---
+++ Android_Me/app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+/**
+ * Created by cfung on 5/2/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
