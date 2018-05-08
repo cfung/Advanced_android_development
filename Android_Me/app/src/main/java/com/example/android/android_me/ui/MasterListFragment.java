a7a2f47165a55d8c4b748a8e7593361f93985937
Chester Fung <cfung@netflix.com>
2018-05-08 16:06:47 -0700

    add new static fragment

---
+++ Android_Me/app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,27 @@
+package com.example.android.android_me.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cfung on 5/8/18.
+ */
+
+public class MasterListFragment extends Fragment{
+
+    public MasterListFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    MasterListAdapter adapter = new MasterListAdapter(Context context, List<Integer> imageIds);
+}
