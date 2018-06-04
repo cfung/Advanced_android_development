a87085d1bb514723afce0f073257780e638ab5ed
Chester Fung <cfung@netflix.com>
2018-06-03 23:07:19 -0700

    update PlantDetailActivity

--- MyGarden/app/src/main/java/com/example/android/mygarden/ui/PlantDetailActivity.java
+++ MyGarden/app/src/main/java/com/example/android/mygarden/ui/PlantDetailActivity.java
@@ -73,6 +73,7 @@ public class PlantDetailActivity extends AppCompatActivity
         contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
         getContentResolver().update(SINGLE_PLANT_URI, contentValues, null, null);
         cursor.close();
+        PlantWateringService.startActionWaterPlants(this);
     }
 
     @Override
