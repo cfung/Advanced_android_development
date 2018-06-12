407f5196e7f9b238cf33d63a701cbfc75264b737
Chester Fung <cfung@netflix.com>
2018-06-11 23:08:31 -0700

    add static final var

--- MyGarden/app/src/main/java/com/example/android/mygarden/ui/PlantWateringService.java
+++ MyGarden/app/src/main/java/com/example/android/mygarden/ui/PlantWateringService.java
@@ -23,7 +23,9 @@ public class PlantWateringService extends IntentService {
 
     // TODO (1): Change ACTION_WATER_PLANTS to ACTION_WATER_PLANT and
     // use EXTRA_PLANT_ID to pass the plant ID to the service and update the query to use SINGLE_PLANT_URI
-    public static final String ACTION_WATER_PLANT = "com.example.android.mygarden.action.water_plants";
+    public static final String ACTION_WATER_PLANT = "com.example.android.mygarden.action.water_plant";
+    public static final String ACTION_UPDATE_PLANT_WIDGET = "com.example.android.mygarden.action.update_plant_widgets";
+    public static final String EXTRA_PLANT_ID = "com.example.android.mygarden.extra.PLANT_ID";
 
     public PlantWateringService(String name) {
         super(name);
